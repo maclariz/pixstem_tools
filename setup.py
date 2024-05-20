11435f2e4999104082c38ecfed22a6d13bf4ac69
maclariz <ian.maclaren@glasgow.ac.uk>
2024-05-20 13:56:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 version_ns = {}
-vpath = convert_path("pixstem_tools/version.py")
+vpath = convert_path("version.py")
 with open(vpath) as version_file:
     exec(version_file.read(), version_ns)
 
